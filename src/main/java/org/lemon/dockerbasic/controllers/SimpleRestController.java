b320fd64934a06b0a14dc4fecde0c7b7bdca7857
Shaun Parry <laughing.programmer@outlook.com>
2023-10-24 10:40:51 +0100

    Integration Test

--- src/main/java/org/lemon/dockerbasic/controllers/SimpleRestController.java
+++ src/main/java/org/lemon/dockerbasic/controllers/SimpleRestController.java
@@ -1,6 +1,7 @@
 package org.lemon.dockerbasic.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.lemon.dockerbasic.model.Person;
 import org.lemon.dockerbasic.services.PersonService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class SimpleRestController {
 
     private final PersonService personService;
 
     @GetMapping("/person/{name}")
     public Person getPerson(@PathVariable String name) {
+        log.info("name: {}", name);
         return personService.findPerson(name);
     }
 
--- src/test/java/org/lemon/dockerbasic/DockerBasicApplicationTests.java
+++ src/test/java/org/lemon/dockerbasic/DockerBasicApplicationTests.java
@@ -1,13 +1,38 @@
 package org.lemon.dockerbasic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class DockerBasicApplicationTests {
+    @Autowired
+    protected WebApplicationContext context;
+
+    protected MockMvc mvc;
+
+    @BeforeEach
+    public void startUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                             .build();
+    }
 
     @Test
-    void contextLoads() {
+    void testBasicIntegrationTest() throws Exception {
+        mvc.perform(get("/person/Andrew"))
+           .andExpect(status().isOk())
+           .andExpect(content().json("{\n" +
+                                             "\t\"firstName\": \"Andrew\",\n" +
+                                             "\t\"lastName\": \"Jones\"\n" +
+                                             "}", true));
     }
 
 }
